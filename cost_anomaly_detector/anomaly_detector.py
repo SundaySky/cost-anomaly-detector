582bdc039fb8eed3ee21c8b002a760dfc7565b15
ohadkap <ohadkap@users.noreply.github.com>
2017-10-22 14:42:47 +0300

    all aws region

--- cost_anomaly_detector/anomaly_detector.py
+++ cost_anomaly_detector/anomaly_detector.py
@@ -116,7 +116,7 @@ def write_anomalies_to_db(anomalies,params):
 
 #%%
 def parse_region(usagetype):
-	regions = {'USW1':'us-west-1', 'USW2':'us-west-2', 'APN1':'ap-northeast-1', 'EUW2':'eu-west-2', 'EUW1':'eu-west-1'}
+	regions = {'APN1':'ap-northeast-1', 'APN2':'ap-northeast-2', 'APS1':'ap-southeast-1', 'APS2':'ap-southeast-2', 'APS3':'ap-south-1', 'CAN1':'ca-central-1', 'EUC1':'eu-central-1', 'EU':'eu-west-1', 'EUW2':'eu-west-2', 'SAE1':'sa-east-1', 'USE1':'us-east-1', 'USE2':'us-east-2', 'USW1':'us-west-1', 'USW2':'us-west-2'}
 	for k,v in regions.items():
 		if usagetype.startswith(k):
 			return v
